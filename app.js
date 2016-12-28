485c73d11fc2922c675e0ea66c89646a6e0c0efd
Clifton Craig <clifton.craig@ge.com>
2016-12-28 14:41:53 -0800

    Adds logic to toggle port based on production deployment.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = 8080;
+var port = process.env.NODE_ENV === "production" ? process.env.port : 8080;
 
 app.get('/', function (req, res) {
     res.send('Hello World!')

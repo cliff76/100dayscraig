8e47bb84469d0cf9d7eed6a74157674648c9a6dd
Clifton Craig <clifton.craig@ge.com>
2017-01-03 09:01:07 -0800

    Adds early facebook integration.

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var port = process.env.NODE_ENV === "production" ? process.env.port : 8080;
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
+app.get('/100', function (req, res) {
+    res.send('Server-side generated content.');
 });
+
+app.use(express.static('www'));
 
 app.listen(port, function () {
     console.log('Example app listening on port ' + port + '!');

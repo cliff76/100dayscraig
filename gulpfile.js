fc8c9bed72280d31f025139bbbf634ae7b378c5c
Clifton Craig <clifton.craig@gmail.com>
2017-01-15 21:04:25 -0800

    Adds gulp-newer plugin to optimize build & only processed changed files.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ gulp = require('gulp');
 var del = require('del');
 var path = require('path');
 var webpack = require('gulp-webpack');
+var newer = require('gulp-newer');
 
 var dir_src = path.resolve(__dirname, 'src');
 var dir_src_server = path.resolve(dir_src, 'server');
@@ -22,12 +23,14 @@ gulp.task('clean', function() {
 
 gulp.task('client', ['server'], function() {
     return gulp.src(path.resolve(dir_js, 'main.js'))
+        .pipe(newer(dir_build_client))
         .pipe(webpack( require('./webpack.config.js') ))
         .pipe(gulp.dest(dir_build_client));
 });
 
 gulp.task('server', function() {
     return gulp.src(dir_src_server + '/*.js')
+        .pipe(newer(dir_build_server))
         .pipe(gulp.dest(dir_build_server));
 });
 
